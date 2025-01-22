03c04cbb6b603fad3e0be598e7ff9e0a2d0e22cd
sweden <ray.likes.pasta@gmail.com>
2025-01-22 21:09:06 +0900

    font

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,14 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
   content: [
     "./app/views/**/*.html.erb",
     "./app/javascript/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Koruri', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [
     require('daisyui'),
   ],
+  daisyui: {
+    themes: ["cupcake"],
+  },
 }
 
